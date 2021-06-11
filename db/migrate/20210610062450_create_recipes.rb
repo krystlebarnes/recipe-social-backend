fbd89bf05b69d1a744075ab1c7a5f478f6a6bbb4
github username <github email address>
2021-06-10 23:25:18 -0600

    added recipe model

---
+++ db/migrate/20210610062450_create_recipes.rb
@@ -0,0 +1,17 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.text :description
+      t.integer :prep_time
+      t.integer :cook_time
+      t.integer :serving_size
+      t.integer :calories
+      t.text :instruction
+      t.belongs_to :author, references: :users, foreign_key: { to_table: :users}
+
+
+      t.timestamps
+    end
+  end
+end

a98245bcd7f2132706c9e53a976b4a96d3aaca20
github username <github email address>
2021-06-12 07:24:30 -0600

    namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :instructions
-  resources :ingredients
-  resources :likes
-  resources :comments
   namespace :api do
     namespace :v1 do
       resources :users
       resources :recipes
+      resources :instructions
+      resources :ingredients
+      resources :likes
+      resources :comments
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

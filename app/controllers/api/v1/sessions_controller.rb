217e42229291f2d16bff8ac610e2bc038cfb5645
github username <github email address>
2021-06-19 16:31:32 -0600

    added logout

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -22,4 +22,11 @@ class Api::V1::SessionsController < ApplicationController
             }
         end
     end
+
+    def destroy
+        session.clear
+        render json: {
+            notice: "You've been logged out."
+        },  status: :ok
+    end
 end

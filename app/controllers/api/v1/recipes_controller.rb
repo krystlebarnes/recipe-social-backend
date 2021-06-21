e49527761dc3bcb7363915bd7714bba0f312fbb6
github username <github email address>
2021-06-21 15:35:57 -0600

    added index method

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::RecipesController < ApplicationController
+
+    def index
+        if params[:search]
+          @recipes = Recipe.search(params[:search])
+        elsif @author = User.find_by_id(params[:user_id])
+          @recipes = @author.recipes
+        else
+          @recipes = Recipe.all
+        end
+
+        render json: RecipeSerializer.new(@recipes)
+    end
+
 end

aa99beda7837718f2c449a9d940484c7de8006f6
github username <github email address>
2021-06-21 15:34:29 -0600

    added recipe with ingredients and instructions and added to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(name: "Krystle", username: "krystlebarnes", password: "password")
+
+Recipe.create(name: "French Toast", description: "Basic and easy french toast", prep_time: 10, cook_time: 10, serving_size: 1, author_id: 1)
+
+Ingredient.create(quantity: "3", unit: "slices", item: "bread", notes: "hawaiian bread recommended", recipe_id: 1)
+Ingredient.create(quantity: "2", item: "eggs", recipe_id: 1)
+Ingredient.create(quantity: "1/2", unit: "cup", item: "milk", recipe_id: 1)
+
+Instruction.create(step: "Heat medium non-stick skillet or griddle to medium heat.", recipe_id: 1)
+Instruction.create(step: "Whisk eggs and milk together.", recipe_id: 1)
+Instruction.create(step: "Dip slice of bread into egg mixture.", recipe_id: 1)
+Instruction.create(step: "Place slice of bread on skillet or griddle and fry both sides until golden brown.", recipe_id: 1)

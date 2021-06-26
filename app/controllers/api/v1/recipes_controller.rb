9020059c18ea63d58805b2b8b79c6a93fdbd5ee1
github username <github email address>
2021-06-26 09:30:19 -0600

    added show, create, update, destroy, and private methods

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :update, :destroy]
+
 
     def index
         if params[:search]
@@ -11,5 +13,55 @@ class Api::V1::RecipesController < ApplicationController
 
         render json: RecipeSerializer.new(@recipes)
     end
+
+    def show
+        render json: RecipeSerializer.new(@recipe)
+    end
+
+    def create
+        @recipe = current_user.recipes.build(recipe_params)
+
+        if @recipe.save
+          render json RecipeSerializer.new(@recipe), status: :created
+        else
+          error_resp = {
+          error: @recipe.error.full_messages.to_sentence
+          }
+          render json: error_resp, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @recipe.update(recipe_params)
+            render json:  RecipeSerializer.new(@recipe), status: :ok
+        else
+            error_resp = {
+                error: @recipe.errors.full_messages.to_sentence
+            }
+            render json: error_resp, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        if @recipe.destroy
+            render json:  { data: "Recipe successfully deleted." }, status: :ok
+        else
+            error_resp = {
+                error: "No recipe found to delete."
+            }
+            render json: error_resp, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :description, :prep_time, :cook_time, :serving_size, :calories, ingredients_attributes: [:_destroy, :id, :quantity, :unit, :item_name, :notes], instructions_attributes: [:_destroy, :id, :step])
+    end
+
 
 end

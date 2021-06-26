8776374324858fc78e3a25757a402f16cd4cf519
github username <github email address>
2021-06-26 09:27:02 -0600

    nested ingredient and instruction routes under recipes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :recipes
-      resources :instructions
-      resources :ingredients
+      resources :recipes do
+        resources :ingredients
+        resources :instructions
+      end
       resources :likes
       resources :comments
     end
